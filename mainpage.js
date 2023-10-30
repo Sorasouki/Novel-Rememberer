f8787ca3349e41cfd17c2a25d4dbb98099dec6cf
Sorasouki <58504567+Sorasouki@users.noreply.github.com>
2023-10-30 10:54:19 +0700

    Update mainpage.js

--- mainpage.js
+++ mainpage.js
@@ -2,89 +2,235 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
 // import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-analytics.js";
 import { getDatabase, ref, onValue, set, remove } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-database.js";
-import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDlfk_bn_pLBzg27pjl3XtGOrrbUOBavYs",
-    authDomain: "book-read-df025.firebaseapp.com",
-    databaseURL: "https://book-read-df025-default-rtdb.asia-southeast1.firebasedatabase.app/",
-    projectId: "book-read-df025",
-    storageBucket: "book-read-df025.appspot.com",
-    messagingSenderId: "206092252656",
-    appId: "1:206092252656:web:e3dcef27ffdcd700929935",
-    measurementId: "G-02BD1X5GF6"
-};
+import { getAuth, onAuthStateChanged, signOut, deleteUser, EmailAuthProvider, updatePassword, reauthenticateWithCredential, updateEmail } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
+import { insertData, sorts, firebaseConfig, dashboardItem } from "./function.js";
 
 // Initialize Firebase
-var date = new Date();
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const db = getDatabase();
 
 insertData();
+sorts();
+dashboardItem();
+
+let dashboard = document.querySelector('.dashboard');
+let dashboardBtn = document.querySelector('.dashboardBtn')
+dashboardBtn.onclick = function () {
+    dashboard.classList.toggle('active');
+}
+
+window.changeEmail = function changeEmail() {
+    const user = auth.currentUser;
+    const credential = EmailAuthProvider.credential(
+        auth.currentUser.email,
+        password
+    )
+    reauthenticateWithCredential(user, credential).then(() => {
+        // User re-authenticated.
+        const response = confirm("Are you sure you want to delete your account?");
+        if (response) {
+            var newEmail = getASecureRandomEmail();
+            updateEmail(auth.currentUser, newEmail).then(() => {
+                // Email updated!
+                // ...
+            }).catch((error) => {
+                // An error occurred
+                // ...
+            });
+        } else {
+            // add code if the user pressed the Cancel button
+            console.log("Cancel was pressed");
+        }
+    }).catch((error) => {
+        // An error ocurred
+        // ...
+    });
+}
+
+window.changePassword = function changePassword() {
+    const user = auth.currentUser;
+    const credential = EmailAuthProvider.credential(
+        auth.currentUser.email,
+        password
+    )
+    reauthenticateWithCredential(user, credential).then(() => {
+        // User re-authenticated.
+        const response = confirm("Are you sure you want to delete your account?");
+        if (response) {
+            var newPassword = getASecureRandomPassword();
+            updatePassword(user, newPassword).then(() => {
+                // Update successful.
+            }).catch((error) => {
+                // An error ocurred
+                // ...
+            });
+        } else {
+            // add code if the user pressed the Cancel button
+            console.log("Cancel was pressed");
+        }
+    }).catch((error) => {
+        // An error ocurred
+        // ...
+    });
+}
+
+window.deleteThisUser = function deleteThisUser() {
+    const user = auth.currentUser;
+    const credential = EmailAuthProvider.credential(
+        auth.currentUser.email,
+        password
+    )
+    reauthenticateWithCredential(user, credential).then(() => {
+        // User re-authenticated.
+        const response = confirm("Are you sure you want to delete your account?");
+        if (response) {
+            deleteUser(user).then(() => {
+                // User deleted.
+                alert('Successfully deleted user');
+            }).catch((error) => {
+                console.log(error);
+                alert('ERROR: ', error);
+                // An error ocurred
+                // ...
+            });
+        } else {
+            // add code if the user pressed the Cancel button
+            console.log("Cancel was pressed");
+        }
+    }).catch((error) => {
+        // An error ocurred
+        // ...
+    });
+}
 
 window.deleteTable = function deleteTable(o) {
     var timespan = o.childNodes[1].value;
-    console.log(timespan);
-    remove((ref(db, 'users/' + uid + '/' + timespan )));
+    remove((ref(db, 'users/' + uid + '/' + timespan)));
 }
+
+window.logout = function logout() {
+    signOut(auth)
+        .then(() => {
+            location.href = 'index.html';
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
+window.changeTable = function changeTable(o) {
+    const titles = o.parentNode.childNodes[0].innerHTML;
+    const titles2 = o.parentNode.childNodes[1].innerHTML;
+    const authors = o.parentNode.childNodes[2].innerHTML;
+    const types = o.parentNode.childNodes[3].innerHTML;
+    const languages = o.parentNode.childNodes[4].innerHTML;
+    const years = o.parentNode.childNodes[5].innerHTML;
+    const statuss = o.parentNode.childNodes[6].innerHTML;
+    const chapters = o.parentNode.childNodes[7].innerHTML;
+    const chapters2 = o.parentNode.childNodes[8].innerHTML;
+    window.changeBtn = function changeBtn(o) {
+        var timespan = o.parentNode.childNodes[10].childNodes[1].value;
+        if (o.parentNode.childNodes[0].innerHTML === titles &&
+            o.parentNode.childNodes[1].innerHTML === titles2 &&
+            o.parentNode.childNodes[2].innerHTML === authors &&
+            o.parentNode.childNodes[3].innerHTML === types &&
+            o.parentNode.childNodes[4].innerHTML === languages &&
+            o.parentNode.childNodes[5].innerHTML === years &&
+            o.parentNode.childNodes[6].innerHTML === statuss &&
+            o.parentNode.childNodes[7].innerHTML === chapters &&
+            o.parentNode.childNodes[8].innerHTML === chapters2) {
+            alert("Change The Data in The Table First!");
+        } else {
+            set(ref(db, 'users/' + uid + '/' + timespan + '/'), {
+                title: o.parentNode.childNodes[0].innerHTML,
+                title2: o.parentNode.childNodes[1].innerHTML,
+                author: o.parentNode.childNodes[2].innerHTML,
+                type: o.parentNode.childNodes[3].innerHTML,
+                language: o.parentNode.childNodes[4].innerHTML,
+                year: o.parentNode.childNodes[5].innerHTML,
+                status: o.parentNode.childNodes[6].innerHTML,
+                chapter: o.parentNode.childNodes[7].innerHTML,
+                chapter2: o.parentNode.childNodes[8].innerHTML,
+            });
+            o.contentEditable = "false";
+            o.setAttribute("contenteditable", "false");
+        }
+    }
+}
+
+const addForm = document.getElementById('add_form');
+addForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    var date = (new Date()).getTime();
+    set(ref(db, 'users/' + uid + '/' + date + '/'), {
+        title: document.getElementById('title').value,
+        title2: document.getElementById('title2').value,
+        author: document.getElementById('author').value,
+        type: document.getElementById('type').value,
+        language: document.getElementById('language').value,
+        year: document.getElementById('year').value,
+        status: document.getElementById('status').value,
+        chapter: document.getElementById('read-chapter').value,
+        chapter2: document.getElementById('latest-chapter').value,
+    });
+    document.getElementById('title').value = "";
+    document.getElementById('title2').value = "";
+    document.getElementById('author').value = "";
+    document.getElementById('year').value = "";
+    document.getElementById('read-chapter').value = "";
+    document.getElementById('latest-chapter').value = "";
+});
 
 var data;
 var uid;
+var password;
 onAuthStateChanged(auth, (user) => {
     if (user) {
+        uid = user.uid;
         const usersRef = ref(db, 'users/' + user.uid);
         onValue(usersRef, (snapshot) => {
-            uid = user.uid;
-            data = Object.entries(snapshot.val());
             var table_body = document.getElementById('entries');
-            table_body.innerHTML = "";
-            for (let i = 0; i < data.length; i++) {
-                document.getElementById("entries").innerHTML += `<tr></input>`
-                    + '<td class="table-data" id="titles">' + data[i][1]["title"]
-                    + '<td class="table-data" id="titles">' + data[i][1]["title2"]
-                    + '<td class="table-data" id="titles">' + data[i][1]["author"]
-                    + '<td class="table-data">' + data[i][1]["type"]
-                    + '<td class="table-data">' + data[i][1]["language"]
-                    + '<td class="table-data">' + data[i][1]["year"]
-                    + '<td class="table-data">' + data[i][1]["status"]
-                    + '<td class="table-data">' + data[i][1]["chapter"]
-                    + '<td class="table-data">' + data[i][1]["chapter2"] + '</td>'
-                    + `<td class="table-data delete-button" onclick="deleteTable(this)">` + 'DELETE'
-                    + `<input type="hidden" name="timespan" value=${data[i][0]}>`;
+            if (snapshot.val() != null || snapshot.val() != undefined) {
+                data = Object.entries(snapshot.val());
+                password = data[data.length - 1][1]['pass'];
+                table_body.innerHTML = "";
+                for (let i = 0; i < data.length; i++) {
+                    if (data[i][0] == 'passData') {
+                        document.getElementById("entries").innerHTML += '';
+                    } else {
+                        document.getElementById("entries").innerHTML += `<tr></input>`
+                            + '<td id="title" contenteditable="true" onclick="changeTable(this)">' + data[i][1]["title"]
+                            + '<td id="title2" contenteditable="true" onclick="changeTable(this)">' + data[i][1]["title2"]
+                            + '<td id="author" contenteditable="true" onclick="changeTable(this)">' + data[i][1]["author"]
+                            + '<td id="type" contenteditable="true" onclick="changeTable(this)">' + data[i][1]["type"]
+                            + '<td id="language" contenteditable="true" onclick="changeTable(this)">' + data[i][1]["language"]
+                            + '<td id="year" contenteditable="true" onclick="changeTable(this)">' + data[i][1]["year"]
+                            + '<td id="status" contenteditable="true" onclick="changeTable(this)">' + data[i][1]["status"]
+                            + '<td id="chapter" contenteditable="true" onclick="changeTable(this)">' + data[i][1]["chapter"]
+                            + '<td id="chapter2" contenteditable="true" onclick="changeTable(this)">' + data[i][1]["chapter2"]
+                            + `<td class="delete-button" onclick="changeBtn(this)">` + 'CHANGE'
+                            + `<td class="delete-button" onclick="deleteTable(this)">` + 'DELETE'
+                            + `<input type="hidden" name="timespan" value=${data[i][0]}>`;
+                    }
+                }
+            }
+            else {
+                table_body.innerHTML = "";
             }
 
-        })
-        let addForm = document.getElementById('add_form');
-        addForm.addEventListener("submit", async (e) => {
-            e.preventDefault();
-            date = (new Date()).getTime();
-            set(ref(db, 'users/' + user.uid + '/' + date + '/'), {
-                title: document.getElementById('title').value,
-                title2: document.getElementById('title2').value,
-                author: document.getElementById('author').value,
-                type: document.getElementById('type').value,
-                language: document.getElementById('language').value,
-                year: document.getElementById('year').value,
-                status: document.getElementById('status').value,
-                chapter: document.getElementById('read-chapter').value,
-                chapter2: document.getElementById('latest-chapter').value,
+            const elements = document.querySelector("#entries")
+            elements.querySelectorAll('td').forEach((element) => {
+                element.classList.add('table-data');
             });
-            console.log('Data succesfully send');
-            document.getElementById('title').value = "";
-            document.getElementById('title2').value = "";
-            document.getElementById('author').value = "";
-            document.getElementById('year').value = "";
-            document.getElementById('read-chapter').value = "";
-            document.getElementById('latest-chapter').value = "";
-        });
+        })
     } else {
         location.href = 'index.html'
     }
-})
+});
 
 
-let logoutBtn = document.querySelector('.logoutBtn');
+/* let logoutBtn = document.querySelector('.logoutBtn');
 logoutBtn.onclick = function () {
     signOut(auth)
         .then(() => {
@@ -95,35 +241,4 @@ logoutBtn.onclick = function () {
         });
 
 }
-
-function insertData() {
-    // OPTION TYPE
-    document.getElementById("type").innerHTML = '<option value="Light Novel">' + 'Light Novel' +
-        '<option value="Published Novel">' + 'Published Novel' +
-        '<option value="Web Novel">' + 'Web Novel' +
-        '<option value="Book">' + 'Book';
-    // OPTION LANGUAGE
-    document.getElementById("language").innerHTML = '<option value="Chinese">' + 'Chinese' +
-        '<option value="Filipino">' + 'Filipino' +
-        '<option value="Indonesian">' + 'Indonesian' +
-        '<option value="Japanese">' + 'Japanese' +
-        '<option value="Khmer">' + 'Khmer' +
-        '<option value="Korean">' + 'Korean' +
-        '<option value="Malaysian">' + 'Malaysian' +
-        '<option value="Thai">' + 'Thai' +
-        '<option value="Vietnamese">' + 'Vietnamese';
-    // OPTION STATUS
-    document.getElementById("status").innerHTML = '<option value="Completed">' + 'Completed' +
-        '<option value="Ongoing">' + 'Ongoing' +
-        '<option value="Hiatus">' + 'Hiatus';
-    // ADD TABLE HEADER
-    document.getElementById("head-entries").innerHTML = '<tr class="box-data">' + '<th class = "table-head table-title">' + "Title" +
-        '<th class = "table-head">' + "Alternative title" +
-        '<th class = "table-head">' + "Author" +
-        '<th class = "table-head">' + "Type" +
-        '<th class = "table-head">' + "Language" +
-        '<th class = "table-head">' + "Year" +
-        '<th class = "table-head">' + "Status" +
-        '<th class = "table-head">' + "Read Chapter" +
-        '<th class = "table-head">' + "Latest Chapter";
-}
+*/
